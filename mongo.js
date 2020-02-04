ce585283580a0cebfc50caf24bba20fcb2eef020
dingo <liparg104@gmail.com>
2020-02-05 00:10:48 +0200

    done some of tasks

--- mongo.js
+++ mongo.js
@@ -98,19 +98,73 @@
 // db.students.find(
 //     {
 //         $and: [
-//             {parents: {$exists: true}},
-//             {"parents.profession": null}
+//             {
+//                 parents: {$exists: true}
+//             },
+//             {
+//                 "parents.profession": null
+//             }
 //         ]
 //     },
 //     {
 //         "parents.name": true,
+//         "parents.profession": true,
 //         "gender": true,
-//         "_id": true
+//         "_id": false
 //     }
 // ).pretty()
 
 // 14) Не працюючих батьків влаштувати офіціантами
+// ?????????????
+
+
 // 15) Вигнати дітей, які мають середній бал менше ніж 2.5
+// db.students.deleteMany({
+//     avgScore: {
+//         $lt: 2.5
+//     }
+// })
+
 // 16) Дітям, батьки яких працюють в освіті ( teacher ) поставити 5
+// db.students.update(
+//     {"parents.profession": "teacher"},
+//     {
+//         $set: {
+//             avgScore: 5
+//         }
+//     },
+//     {
+//         multi: true
+//     }
+// )
+
 // 17) Знайти дітей які вчаться в початковій школі (до 5 класу) і вивчають фізику ( physics )
+// db.students.find({
+//     $and: [
+//         {class: {
+//             $lte: 5
+//             }},
+//         {
+//             lessons: "physics"
+//         }
+//     ]
+// }).pretty()
+
 // 18) Знайти найуспішніший клас
+// db.students.aggregate([
+//     {
+//         $match: {avgScore: {$exists: true}}
+//     },
+//     {
+//         $group: {
+//             "_id": "$class",
+//             "score": {$avg: "$avgScore"}
+//         }
+//     },
+//     {
+//         $sort: {score: -1}
+//     },
+//     {
+//         $limit: 1
+//     }
+// ])
